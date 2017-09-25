c2a31d8464c2542a1ef14ece9c430253c3044cf5
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-24 21:28:37 -0700

    msm8996: Add DisplayEventReceiver and GraphicBuffer shim for camera
    These functions changed between N and O so forward the old functions to
    their new counterparts.
    
    Change-Id: Id66dd0cc5c37e09f576b9502e7498a2f63fa04f5

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,6 +16,24 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES := \
+    camera_shim.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libgui \
+    libui
+
+LOCAL_C_INCLUDES := \
+    frameworks/native/include
+
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 32
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := MediaBuffer.c
 
 LOCAL_SHARED_LIBRARIES := libstagefright_foundation
--- msm8996.mk
+++ msm8996.mk
@@ -128,6 +128,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl \
+    libshim_camera \
     Snap
 
 # CNE
